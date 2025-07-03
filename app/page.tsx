7038484d64e82cf31543badffdc6ec60ab11fe18
Agnibha Nanda <agnibhananda@gmail.com>
2025-07-03 20:16:46 +0530

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <h1>Hello World</h1>
+      <Button>Click me</Button> 
+      </main>
+  );
+}
